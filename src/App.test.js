d327f13aedf158efa46d8ee39ea7ec839a79283f
Radgus <dominguezradgus@gmail.com>
2022-01-31 19:56:01 -0600

    testing-library example added

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders label', () => {
+  render(<App />);
+  const exampleInput = screen.getByLabelText(/Example/i)
+  expect(exampleInput).toBeInTheDocument();
+});
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+    <h1>This is a Header</h1> 
+    <label htmlFor="example">Example</label>
+    <input id='example' name='example'/>
+    </>
+  )
+}
+
+export default Header;
